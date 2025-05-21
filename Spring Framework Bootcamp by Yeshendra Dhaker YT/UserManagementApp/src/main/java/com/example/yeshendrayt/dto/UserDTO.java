54812d6ce0cede5a0e2204e73ba9ce9926f7daa3
theahmed619 <theahmed619@gmail.com>
2025-05-21 17:19:11 +0530

    complete user management app backend

---
+++ Spring Framework Bootcamp by Yeshendra Dhaker YT/UserManagementApp/src/main/java/com/example/yeshendrayt/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.example.yeshendrayt.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserDTO {
+	
+	private Long id;
+	
+	@NotBlank
+	@Size(min=3, max=50)
+	private String username;
+	
+	@NotBlank
+	@Size(max=7)
+	@Email
+	private String email;
+
+}
